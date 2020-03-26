8674ebd33db54dd0a8996e92d0633335b0da2409
Ryan Hunter <rymalhunt@gmail.com>
2020-03-26 08:35:22 -0600

    Remove unnecessary ENV variables

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,12 +1,12 @@
 Clearance.configure do |config|
   # config.allow_sign_up = true
-  config.cookie_domain = ENV['COOKIE_DOMAIN']
+  # config.cookie_domain = ".kindnesskillscovid.com"
   # config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   # config.cookie_name = "remember_token"
   # config.cookie_path = "/"
   config.routes = false
   # config.httponly = false
-  config.mailer_sender = ENV['MAILER_SENDER']
+  config.mailer_sender = "admin@kindnesskillscovid.com"
   # config.password_strategy = Clearance::PasswordStrategies::BCrypt
   # config.redirect_url = "/"
   config.rotate_csrf_on_sign_in = true
